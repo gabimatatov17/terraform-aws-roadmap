1a0e32ea62cf99b32238adf85adf013945c88ccb
gabimatatov17 <gabim435@gmail.com>
2025-04-14 22:26:51 +0300

    renamed directory

---
+++ 02-aws-core/vpc-terraform/variables.tf
@@ -0,0 +1,30 @@
+variable "aws_region" {
+  description = "The AWS region to deploy the resources in"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Progetct name prefix"
+  type        = string
+}
+
+variable "cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+}
