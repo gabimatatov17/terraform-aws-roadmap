60bf5eb9e2666bf4530b9820af5fb3a114a24c20
gabimatatov17 <gabim435@gmail.com>
2025-04-17 22:31:01 +0300

    Added S3 backend for state management

--- 02-aws-core/vpc-terraform/backend/s3-backend.tf
+++ 02-aws-core/vpc-terraform/backend/s3-backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "gabi-terraform-state"
-    key    = "phase2/terraform.tfstate"
-    region = "us-east-1"
-    encrypt = true
+    bucket       = "gabi-terraform-state"
+    key          = "phase2/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
