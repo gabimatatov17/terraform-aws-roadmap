9ff22ed6362320e8baff17e7f9628986ac9f945b
gabimatatov17 <gabim435@gmail.com>
2025-04-17 22:48:05 +0300

    Added S3 backend for state management

--- 02-aws-core/vpc-terraform/main.tf
+++ 02-aws-core/vpc-terraform/main.tf
@@ -1,3 +1,14 @@
+
+#### Terraform Configuration and Providers ####
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.22.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
